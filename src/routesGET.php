5d5df27331cb4654f6ca42ccd86d12b9c6d26384
Clément <xSlOom@users.noreply.github.com>
2017-03-13 09:44:30 +0100

    Update routesGET.php

--- src/routesGET.php
+++ src/routesGET.php
@@ -1,29 +1,29 @@
 <?php
 
 $app->get('/', function ($request, $response, $args) {
-    return $this->renderer->render($response, 'index.phtml');
+    return $this->view->render($response, 'index.phtml');
 });
 
 $app->get("/dashboard", function($request, $response, $args) {
-    return $this->renderer->render($response, 'dashboard.phtml');
+    return $this->view->render($response, 'dashboard.phtml');
 });
 
 $app->get("/cordonnees", function($request, $response, $args) {
-    return $this->renderer->render($response, 'cordonnees.phtml');
+    return $this->view->render($response, 'cordonnees.phtml');
 });
 
 $app->get("/formation", function($request, $response, $args) {
-    return $this->renderer->render($response, 'formation.phtml');
+    return $this->view->render($response, 'formation.phtml');
 });
 
 $app->get("/stagiaire", function($request, $response, $args) {
-    return $this->renderer->render($response, 'stagiaire.phtml');
+    return $this->view->render($response, 'stagiaire.phtml');
 });
 
 $app->get("/jvisite", function($request, $response, $args) {
-    return $this->renderer->render($response, 'jvisite.phtml');
+    return $this->view->render($response, 'jvisite.phtml');
 });
 
 $app->get("/docpdf", function($request, $response, $args) {
-    return $this->renderer->render($response, 'docpdf.phtml');
+    return $this->view->render($response, 'docpdf.phtml');
 });
