5dc44ad189535a5530aa73a938e401fc1252fcaa
[xSlOom@xat]$ <xSlOom@users.noreply.github.com>
2017-03-14 11:12:42 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -16,8 +16,6 @@ session_start();
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
-
-
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
 
