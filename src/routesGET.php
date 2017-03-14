cb22e3f9e4fa07583676f0cb22ddd61ffbb7d314
[xSlOom@xat]$ <xSlOom@users.noreply.github.com>
2017-03-14 15:37:55 +0100

    Update routesGET.php

--- src/routesGET.php
+++ src/routesGET.php
@@ -1,41 +1,69 @@
 <?php
 
 $app->get('/', function ($request, $response, $args) {
-    return $this->view->render($response, 'index.phtml');
+    if (!login::VerifSession()) {
+        return $this->view->render($response, "index.phtml");
+    } else {
+        return $this->view->render($response, 'dashboard.phtml');
+    }
 });
 
 $app->get("/dashboard", function($request, $response, $args) {
-    return $this->view->render($response, 'dashboard.phtml', [
-        "dashboard" => true
-    ]);
+    if (!login::VerifSession()) {
+        return $this->view->render($response, "index.phtml");
+    } else {
+        return $this->view->render($response, 'dashboard.phtml', [
+            "dashboard" => true
+        ]);
+    }
 });
 
-$app->get("/coordonnees", function($request, $response, $args) {
-    return $this->view->render($response, 'cordonnees.phtml', [
-        "coordonnee" => true
-    ]);
+$app->get("/cordonnees", function($request, $response, $args) {
+    if (!login::VerifSession()) {
+        return $this->view->render($response, "index.phtml");
+    } else {
+        return $this->view->render($response, 'cordonnees.phtml', [
+            "coordonnee" => true
+        ]);
+    }
 });
 
 $app->get("/formation", function($request, $response, $args) {
-    return $this->view->render($response, 'formation.phtml', [
-        "formation" => true
-    ]);
+    if (!login::VerifSession()) {
+        return $this->view->render($response, "index.phtml");
+    } else {
+        return $this->view->render($response, 'formation.phtml', [
+            "formation" => true
+        ]);
+    }
 });
 
 $app->get("/stagiaire", function($request, $response, $args) {
-    return $this->view->render($response, 'stagiaire.phtml', [
-        "stagiaire" => true
-    ]);
+    if (!login::VerifSession()) {
+        return $this->view->render($response, "index.phtml");
+    } else {
+        return $this->view->render($response, 'stagiaire.phtml', [
+            "stagiaire" => true
+        ]);
+    }
 });
 
 $app->get("/jvisite", function($request, $response, $args) {
-    return $this->view->render($response, 'jvisite.phtml', [
-        "jvisite" => true
-    ]);
+    if (!login::VerifSession()) {
+        return $this->view->render($response, "index.phtml");
+    } else {
+        return $this->view->render($response, 'jvisite.phtml', [
+            "jvisite" => true
+        ]);
+    }
 });
 
 $app->get("/docpdf", function($request, $response, $args) {
-    return $this->view->render($response, 'docpdf.phtml', [
-        "docpdf" => true
-    ]);
+    if (!login::VerifSession()) {
+        return $this->view->render($response, "index.phtml");
+    } else {
+        return $this->view->render($response, 'docpdf.phtml', [
+            "docpdf" => true
+        ]);
+    }
 });
