40c4dc8182d89c6da082a23fa724b39de4ff8b80
RomainMustang <romain.g@codeur.online>
2017-03-14 10:42:31 +0100

    Ajout du post /coordonnees

--- src/routesPOST.php
+++ src/routesPOST.php
@@ -2,3 +2,20 @@
 /*
  * POST ROUTE ARE HERE
  */
+
+$app->post('/coordonnees', function($request, $response){
+	
+	if(empty($_POST['nomOnline']) && empty($_POST['adresseOnline']) && empty($_POST['telephoneOnline'])) {
+    	if(($_POST['nomOnline'] !== '') && ($_POST['adresseOnline'] !== '') && ($_POST['telephoneOnline'] !=='')) {
+        	$reponse = 'ok';
+    	} else {
+        	$reponse = 'Les champs sont vides';
+    	}
+	} else {
+    	$reponse = 'Tous les champs ne sont setter';
+}
+echo json_encode(['reponse' => $reponse]);
+
+});
+
+?>
