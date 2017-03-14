03d536c93c183360a5bddfaf8df1d2430d6f7020
[xSlOom@xat]$ <xSlOom@users.noreply.github.com>
2017-03-14 11:10:04 +0100

    Update routesPOST.php

--- src/routesPOST.php
+++ src/routesPOST.php
@@ -3,6 +3,24 @@
  * POST ROUTE ARE HERE
  */
 
+$app->post("/", function($request, $response, $args) {
+    $erreur = [];
+    $json   = json_decode($request->getBody());
+    if (($json->email == null || $json->pass == null || $json->email !== $this->login[0] || $json->pass !== $this->login[1])) {
+        $erreur = [
+            "erreur" => true,
+            "message" => "Impossible de se connecter. (Erreur d'email/mot de passe)"
+        ];
+    } else {
+        $erreur = [
+            "erreur" => false,
+            "message" => "Vous êtes maintenant connecté! ;)"
+        ];
+    }
+    $response->withHeader('Content-type', 'application/json');
+    return $response->withJson($erreur, 200, JSON_PRETTY_PRINT);
+});
+
 $app->post('/coordonnees', function($request, $response){
 	
 	if(empty($_POST['nomOnline']) && empty($_POST['adresseOnline']) && empty($_POST['telephoneOnline'])) {
