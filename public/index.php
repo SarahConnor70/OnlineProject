9237944fe1ab102404185f293efc01a74c118c9d
Clément <xSlOom@users.noreply.github.com>
2017-03-14 09:36:05 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -10,12 +10,13 @@ if (PHP_SAPI == 'cli-server') {
 }
 
 require __DIR__ . '/../vendor/autoload.php';
-
 session_start();
 
 // Instantiate the app
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
+
+
 
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
@@ -25,6 +26,7 @@ require __DIR__ . '/../src/middleware.php';
 
 // Models
 require __DIR__ . '/../models/database.class.php';
+require __DIR__ . '/../models/createStagiaire.php';
 
 // Register routes
 require __DIR__ . '/../src/routesGET.php';
