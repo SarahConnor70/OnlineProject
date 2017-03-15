10b250c5e324aa88f06b6fdaf4376fc19dece3b6
RomainMustang <romain.g@codeur.online>
2017-03-15 10:44:17 +0100

    Dernière mise à jour de modelCoord.php

--- models/modelCoord.php
+++ models/modelCoord.php
@@ -1,14 +1,27 @@
 <?php
 
-require_once('database.class.php');
-
 	class ModelCoord{
-		public $nom, $adresse, $telephone;
 
 		public static function setEntreprise($donnees){
-			$this->nom       = $donnees[0];
-			$this->adresse   = $donnees[1];
-			$this->telephone = $donnees[2];
+
+			$query  = "UPDATE coordonnees SET nomEntreprise = :nomEntreprise,
+											  adresseEntreprise = :adresseEntreprise,
+											  telephoneEntreprise = :telephoneEntreprise
+					   WHERE id=1";
+			$execute = Database::bdd()->prepare($query);
+			$execute->bindParam(":nomEntreprise"      , $donnees[0]);
+			$execute->bindParam(":adresseEntreprise"  , $donnees[1]);
+			$execute->bindParam(":telephoneEntreprise", $donnees[2]);
+
+			return $execute->execute(); 			
+		}
+
+		public static function getEntreprise(){
+
+			$query = "SELECT * FROM coordonnees";
+
+			$execute = Database::bdd()->query($query);
+			return $execute->fetch();
 		}
 	}
 
