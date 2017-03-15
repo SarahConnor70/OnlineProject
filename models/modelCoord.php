a048d69bf016c2343419ac63c52c23bf78ae5e70
[xSlOom@xat]$ <xSlOom@users.noreply.github.com>
2017-03-15 14:01:42 +0100

    Update modelCoord.php

--- models/modelCoord.php
+++ models/modelCoord.php
@@ -1,28 +1,17 @@
 <?php
+class ModelCoord {
+    public static function setEntreprise($donnees){
+        $query  = "UPDATE coordonnees SET nomEntreprise = :nomEntreprise, adresseEntreprise = :adresseEntreprise, telephoneEntreprise = :telephoneEntreprise WHERE id=1";
+        $execute = Database::bdd()->prepare($query);
+        $execute->bindParam(":nomEntreprise"      , $donnees[0]);
+        $execute->bindParam(":adresseEntreprise"  , $donnees[1]);
+        $execute->bindParam(":telephoneEntreprise", $donnees[2]);
+        return $execute->execute();
+    }
 
-	class ModelCoord{
-
-		public static function setEntreprise($donnees){
-
-			$query  = "UPDATE coordonnees SET nomEntreprise = :nomEntreprise,
-											  adresseEntreprise = :adresseEntreprise,
-											  telephoneEntreprise = :telephoneEntreprise
-					   WHERE id=1";
-			$execute = Database::bdd()->prepare($query);
-			$execute->bindParam(":nomEntreprise"      , $donnees[0]);
-			$execute->bindParam(":adresseEntreprise"  , $donnees[1]);
-			$execute->bindParam(":telephoneEntreprise", $donnees[2]);
-
-			return $execute->execute(); 			
-		}
-
-		public static function getEntreprise(){
-
-			$query = "SELECT * FROM coordonnees";
-
-			$execute = Database::bdd()->query($query);
-			return $execute->fetch();
-		}
-	}
-
-?>
+    public static function getEntreprise() {
+        $query = "SELECT * FROM coordonnees";
+        $execute = Database::bdd()->query($query);
+        return $execute->fetch();
+    }
+}
