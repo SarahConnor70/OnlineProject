3225b0cf342721dd62d8481c5d13bed77e294b5d
[xSlOom@xat]$ <xSlOom@users.noreply.github.com>
2017-03-15 16:59:40 +0100

    Update formation.php

--- models/formation.php
+++ models/formation.php
@@ -1,60 +1,45 @@
 <?php
 
-	class ModelFormation{
+class ModelFormation{
 
-		public static function setFormation($info){
+    public static function setFormation($info){
+        $query = "INSERT INTO formations(debut, fin, placeRegion, placeSupp, intitule, titre, promo) VALUES (:debut, :fin, :placeRegion, :placeSupp, :intitule, :titre, :promo)";
+        $execute    = DataBase::bdd()->prepare($query);
+        $execute->bindParam(':debut', $info[0]);
+        $execute->bindParam(':fin', $info[1]);
+        $execute->bindParam(':placeRegion', $info[2]);
+        $execute->bindParam(':placeSupp', $info[3]);
+        $execute->bindParam(':intitule', $info[4]);
+        $execute->bindParam(':titre', $info[5]);
+        $execute->bindParam(':promo', $info[6]);
 
-			// Récupère l'année de début et de fin
-			$annee1 = explode("-", $info[0]);
-			$annee2 = explode("-", $info[1]);
+        return $execute->execute();
+    }
 
-			// Si l'année est la même la promo est l'année de début
-			if($annee1[0] == $annee2[0]){
-				$promo = $annee1[0];
-			}
+    public static function getId(){
+        $query = "SELECT id FROM formations order by id desc limit 1";
 
-			// Sinon la promo est année1 année2
-			else{
-				$promo = $annee1[0] . " " . $annee2[0];
-			}
+        $execute = Database::bdd()->query($query);
+        return $execute->fetch();
+    }
 
-			$query = "INSERT INTO formations(debut, fin, placeRegion, placeSupp, intitule, titre, promo) VALUES (:debut, :fin, :placeRegion, :placeSupp, :intitule, :titre, :promo)";
-			$execute    = DataBase::bdd()->prepare($query);
-        	$execute->bindParam(':debut', $info[0]);
-        	$execute->bindParam(':fin', $info[1]);
-        	$execute->bindParam(':placeRegion', $info[2]);
-        	$execute->bindParam(':placeSupp', $info[3]);
-        	$execute->bindParam(':intitule', $info[4]);
-        	$execute->bindParam(':titre', $info[5]);
-        	$execute->bindParam(':promo', $promo);
+    public static function getFormation($bool){
 
-        	return $execute->execute();
-		}
+        // Si la formation est la dernière
+        if($bool){
+            $id = self::getId()['id'];
+        }
+        // Si la formation est l'avant dernière
+        else{
+            $id = self::getId()['id'] - 1;
+        }
+        $query = "SELECT * FROM formations WHERE id=$id";
 
-		public static function getId(){
-			$query = "SELECT id FROM formations order by id desc limit 1";
+        $execute = Database::bdd()->query($query);
+        return $execute->fetch();
 
-			$execute = Database::bdd()->query($query);
-			return $execute->fetch();
-		}
+    }
 
-		public static function getFormation($bool){
-
-			// Si la formation est la dernière
-			if($bool){
-				$id = self::getId()['id'];
-			}
-			// Si la formation est l'avant dernière
-			else{
-				$id = self::getId()['id'] - 1;
-			}
-			$query = "SELECT * FROM formations WHERE id=$id";
-
-			$execute = Database::bdd()->query($query);
-			return $execute->fetch();
-
-		}
-
-	}
+}
 
 ?>
