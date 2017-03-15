bb8e67ab19499018d784ac8c1bc4d4c1d7293544
RomainMustang <romain.g@codeur.online>
2017-03-15 16:38:10 +0100

    Ajout du click pour ajouter une formation

--- public/assets/js/communication.js
+++ public/assets/js/communication.js
@@ -23,6 +23,57 @@ $(document).ready(function() {
             })
         }
     });
+    
+    //formation
+    $('#maj').on('click', function (e){
+        e.preventDefault();
+        var debut       = $('#dateDebut').val();
+        var fin         = $('#dateFin').val();
+        var placeRegion = $("#placeRegion").val();
+        var placeSupp   = $("#placeSupp").val();
+        var intitule    = $("#intitule").val();
+        var titre       = $('#titre').val();
+
+        // Récupère l'année de début et de fin
+        var annee1 = debut.split("-");
+        var annee2 = fin.split("-");
+        var promo;
+
+        // Si l'année est la même la promo est l'année de début
+        if(annee1[0] == annee2[0]){
+            promo = annee1[0];
+        }
+
+        // Sinon la promo est année1 année2
+        else{
+            promo = annee1[0] + " " + annee2[0];
+        }
+
+        if(debut == '' || fin == '' || placeRegion == '' || placeSupp == '' || intitule == '' || titre == ''){
+            alert('Les champs ne sont pas tous rempli');
+        }
+        else{
+            $.ajax({
+                url :'/formation',
+                type: 'POST',
+                data: "dateDebut=" + debut + "&dateFin=" + fin + "&placeRegion=" + placeRegion + "&placeSupp=" + placeSupp + "&intitule=" + intitule + "&titre=" + titre + "&promo=" + promo,
+                dateType: 'json',
+                success: function(json){
+                    var jsons = JSON.parse(json);
+                    if(jsons.reponse == 'ok'){
+                        faireNotif("json ok pour envoie des données", "primary");
+                    }
+                    else{
+                        faireNotif("Problème json pour envoie des données", "error");
+                    }
+                },
+                error: function(error){
+                    faireNotif("Un problème est survenu ...", "error");
+                }
+            });
+        }
+    });
+    
     //coordonnées
     $('#online').on('submit', function(e) {
         e.preventDefault();  // Le formulaire ne s'envoie pas
