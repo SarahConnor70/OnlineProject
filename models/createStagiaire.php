57433066ffe6cf52f850171590d589aaf0ab4385
RomainMustang <romain.g@codeur.online>
2017-03-17 15:55:46 +0100

    modif de la fonction insertStagiaire()

--- models/createStagiaire.php
+++ models/createStagiaire.php
@@ -1,20 +1,22 @@
 <?php
 
-class stagiaire {
-    // Insererun nouveau stagiaire
-    public static function InsertStagiaire($nom, $prenom, $cp, $ville, $email, $telephone, $promo) {
-        $query      = "INSERT INTO stagiaires (nom, prenom, cp, ville, mail, telephone, promo) VALUES(:nom, :prenom, :cp, :ville, :mail, :telephone, :promo)";
-        $execute    = DataBase::bdd()->prepare($query);
-        $execute->bindParam(':nom', $nom);
-        $execute->bindParam(':prenom', $prenom);
-        $execute->bindParam(':cp', $cp);
-        $execute->bindParam(':ville', $ville);
-        $execute->bindParam(':mail', $email);
-        $execute->bindParam(':telephone', $telephone);
+class Stagiaire {
+    // Inserer un nouveau stagiaire
+    public static function insertStagiaire($stagiaire) {
+        $query      = "INSERT INTO stagiaires (nom, prenom, adresse, cp, ville, mail, telephone, promo, accepter) VALUES(:nom, :prenom, :adresse, :cp, :ville, :mail, :telephone, :promo, :accepter)";
+        $promo = ModelFormation::getLastPromo()['promo'];
+        $execute = DataBase::bdd()->prepare($query);
+        $execute->bindParam(':nom', $stagiaire[0]);
+        $execute->bindParam(':prenom', $stagiaire[1]);
+        $execute->bindParam(':telephone', $stagiaire[2]);
+        $execute->bindParam(':mail', $stagiaire[3]);
+        $execute->bindParam(':adresse', $stagiaire[4]);
+        $execute->bindParam(':cp', $stagiaire[5]);
+        $execute->bindParam(':ville', $stagiaire[6]);
+        $execute->bindParam(':accepter', $stagiaire[7]);
         $execute->bindParam(':promo', $promo);
         return $execute->execute();
     }
-
     // Recuperer un stagiare depuis la base de données
     public static function RecupStagiaire($id){
         $query  = DataBase::bdd()->prepare("SELECT * from stagiaires ORDER by id DESC");
