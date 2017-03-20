5d1a3fcae74b21d49ce55806ac968a70cd6cbbc2
SarahConnor70 <sarah.p@codeur.online>
2017-03-20 15:43:20 +0100

    sql avec table resultat test

--- projetOnline.sql
+++ projetOnline.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost
--- Généré le :  Mer 15 Mars 2017 à 10:48
+-- Généré le :  Lun 20 Mars 2017 à 15:41
 -- Version du serveur :  10.1.21-MariaDB
--- Version de PHP :  7.0.15
+-- Version de PHP :  7.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `projetOnline`
 --
-
-CREATE DATABASE 'projetOnline';
 
 -- --------------------------------------------------------
 
@@ -62,6 +60,46 @@ CREATE TABLE `formations` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `resultatTest`
+--
+
+CREATE TABLE `resultatTest` (
+  `id` int(11) NOT NULL,
+  `date` varchar(10) NOT NULL,
+  `connuFormation` varchar(255) NOT NULL,
+  `age` int(2) NOT NULL,
+  `prescription` varchar(255) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `prescripteur` varchar(255) NOT NULL,
+  `contreIndic` varchar(255) NOT NULL,
+  `commentaire` varchar(255) NOT NULL,
+  `id_stagiaire` int(11) NOT NULL,
+  `resultatNiveau` varchar(255) NOT NULL,
+  `resultatFormation` varchar(255) NOT NULL,
+  `resultatExperience` varchar(255) NOT NULL,
+  `pointNiveau` varchar(5) NOT NULL,
+  `pointFormation` varchar(5) NOT NULL,
+  `pointExperience` varchar(5) NOT NULL,
+  `commentaire1` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `resultatTest`
+--
+
+INSERT INTO `resultatTest` (`id`, `date`, `connuFormation`, `age`, `prescription`, `status`, `prescripteur`, `contreIndic`, `commentaire`, `id_stagiaire`, `resultatNiveau`, `resultatFormation`, `resultatExperience`, `pointNiveau`, `pointFormation`, `pointExperience`, `commentaire1`) VALUES
+(1, '12/03/2017', 'oui', 45, 'non', 'oui', 'oui', 'non', 'non', 0, '', '', '', '', '', '', ''),
+(2, '23/05/2017', 'oui', 78, 'oui', 'oui', 'oui', 'non', 'non', 0, '', '', '', '', '', '', ''),
+(3, '23/05/2017', 'oui', 78, 'oui', 'oui', 'oui', 'non', 'non', 0, '', '', '', '', '', '', ''),
+(4, '23/05/2017', 'oui', 78, 'oui', 'oui', 'oui', 'non', 'non', 0, '', '', '', '', '', '', ''),
+(5, '23/10/2018', 'non', 26, 'non', 'oui', 'non', 'non', 'oui', 0, '', '', '', '', '', '', ''),
+(6, '23/10/2018', 'non', 26, 'non', 'oui', 'non', 'non', 'oui', 0, '', '', '', '', '', '', ''),
+(7, '9/12/2347', 'non', 34, 'noui', 'nkpl', 'ouo', 'non', 'oui', 0, '', '', '', '', '', '', ''),
+(8, '21/03/2017', 'xcvbn', 56, 'cvbn', 'v nbn', 'vhbkjnlk', 'hkj', 'cvbnjk', 0, 'oui', 'oui', 'non', '1', '2', '1', 'gcfnbnn');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `stage`
 --
 
@@ -87,7 +125,6 @@ CREATE TABLE `stagiaires` (
   `id` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
-  `adresse` varchar(150) NOT NULL,
   `cp` int(5) NOT NULL,
   `ville` varchar(50) NOT NULL,
   `mail` varchar(100) NOT NULL,
@@ -110,6 +147,12 @@ ALTER TABLE `coordonnees`
 -- Index pour la table `formations`
 --
 ALTER TABLE `formations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `resultatTest`
+--
+ALTER TABLE `resultatTest`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -138,6 +181,11 @@ ALTER TABLE `coordonnees`
 --
 ALTER TABLE `formations`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `resultatTest`
+--
+ALTER TABLE `resultatTest`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT pour la table `stage`
 --
