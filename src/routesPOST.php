31dfb5db9b960436f18d11d91463d2f64a3c0f07
RomainMustang <romain.g@codeur.online>
2017-03-21 13:50:43 +0100

    Update routesPOST.php

--- src/routesPOST.php
+++ src/routesPOST.php
@@ -43,8 +43,51 @@ $app->post('/coordonnees', function($request, $response){
 
 $app->post('/stagiaire', function($request, $response)
 {
-    if(!empty($_POST['date']) && !empty($_POST['connuFormation']) && !empty($_POST['age']) && !empty($_POST['prescription']) && !empty($_POST['status']) && !empty($_POST['prescripteur']) && !empty($_POST['contreIndic']) && !empty($_POST['commentaire']) && !empty($_POST['resultatNiveau']) && !empty($_POST['resultatFormation']) && !empty($_POST['resultatExperience']) && !empty($_POST['pointNiveau']) && !empty($_POST['pointFormation']) && !empty($_POST['pointExperience']) && !empty($_POST['commentaire1'])){
+    
+    if(!empty($_POST['prenomRecherche']) && !empty($_POST['nomRecherche'])){
+        $reponse = 'ok';
 
+        $resultat = Stagiaire::rechercheStagiaire($_POST['prenomRecherche'], $_POST['nomRecherche']);
+        $response->withHeader('Content-type', 'application/json');
+        return $response->withJson(['recherche' => $resultat, 'reponse' => $reponse], 200, JSON_PRETTY_PRINT);
+    }
+
+    else if(!empty($_POST['nomEntreprise']) && !empty($_POST['adresseEntreprise']) && !empty($_POST['telephoneEntreprise']) && !empty($_POST['nomTuteur'])){
+        $reponse = 'ok';
+        $infoEntreprise = [];
+        $infoEntreprise[0] = $_POST['nomEntreprise'];
+        $infoEntreprise[1] = $_POST['adresseEntreprise'];
+        $infoEntreprise[2] = $_POST['telephoneEntreprise'];
+        $infoEntreprise[3] = $_POST['nomTuteur'];
+        echo json_encode(['reponse' => $reponse]);
+    }
+
+    else if(!empty($_POST['nomStagiaire']) && !empty($_POST['prenomStagiaire']) && !empty($_POST['telephoneStagiaire']) && !empty($_POST['mailStagiaire']) && !empty($_POST['adresseStagiaire']) && !empty($_POST['cpStagiaire']) && !empty($_POST['villeStagiaire'])){
+        $reponse = 'ok';
+        $stagiaire = [];
+        $stagiaire[0] = $_POST['nomStagiaire'];
+        $stagiaire[1] = $_POST['prenomStagiaire'];
+        $stagiaire[2] = $_POST['telephoneStagiaire'];
+        $stagiaire[3] = $_POST['mailStagiaire'];
+        $stagiaire[4] = $_POST['adresseStagiaire'];
+        $stagiaire[5] = $_POST['cpStagiaire'];
+        $stagiaire[6] = $_POST['villeStagiaire'];
+        $stagiaire[7] = $_POST['accepter'];
+        if(Stagiaire::rechercheStagiaire($stagiaire[1],$stagiaire[0]) == null){
+            Stagiaire::insertStagiaire($stagiaire);
+        }
+        else{
+            Stagiaire::modifStagiaire($stagiaire);
+        }
+        echo json_encode(['reponse' => $reponse]);
+    }
+    else{
+        $reponse = 'Les champs sont vides';
+        echo json_encode(['reponse' => $reponse]);
+    }
+    
+    if(!empty($_POST['date']) && !empty($_POST['connuFormation']) && !empty($_POST['age']) && !empty($_POST['prescription']) && !empty($_POST['status']) && !empty($_POST['prescripteur']) && !empty($_POST['contreIndic']) && !empty($_POST['commentaire']) && !empty($_POST['resultatNiveau']) && !empty($_POST['resultatFormation']) && !empty($_POST['resultatExperience']) && !empty($_POST['pointNiveau']) && !empty($_POST['pointFormation']) && !empty($_POST['pointExperience']) && !empty($_POST['commentaire1'])){
+        
         $reponse='ok';
         $resultatTest=[];
         $resultatTest[0]= $_POST['date'];
