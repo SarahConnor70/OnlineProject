ad08bcc983ef8bfb769c27600e1e26f48b6e0d81
RomainMustang <romain.g@codeur.online>
2017-03-21 13:56:58 +0100

    Update createStagiaire.php

--- models/createStagiaire.php
+++ models/createStagiaire.php
@@ -18,7 +18,7 @@ class Stagiaire {
         return $execute->execute();
     }
 
-    // Recuperer un stagiare depuis la base de données
+    // Recuperer un stagiaire depuis la base de données
     public static function recupStagiaire(){
         $promo  = ModelFormation::getLastPromo()["promo"];
         $query  = DataBase::bdd()->prepare("SELECT * from stagiaires WHERE promo = :promo ORDER by id DESC");
@@ -52,22 +52,34 @@ class Stagiaire {
 
     }
 
-    public static function modifStagiaire(){
+    public static function rechercheStagiaire($prenom, $nom){
+        $query = DataBase::bdd()->prepare("SELECT * FROM stagiaires WHERE prenom =:prenom AND nom =:nom");
+        $query->bindParam(":prenom", $prenom);
+        $query->bindParam(":nom", $nom);
+        $query->execute();
+
+        $fetch = $query->fetch();
+        return $fetch;
+    }
+
+    public static function modifStagiaire($stagiaire){
         $query  = "UPDATE stagiaires SET cp = :cp,
                                          ville = :ville,
                                          mail = :mail,
                                          telephone = :telephone,
-                                         promo = :promo,
+                                         adresse = :adresse,
                                          accepter = :accepter
                        WHERE nom = :nom AND prenom = :prenom";
 
-        $execute = DataBase::bdd()->prepapre($query);
-        $execute->bindParam(':cp', $cp);
-        $execute->bindParam(':ville', $ville);
-        $execute->bindParam(':mail', $email);
-        $execute->bindParam(':telephone', $telephone);
-        $execute->bindParam(':promo', $promo);
-        $execute->bindParam(':accepter', $accepter);
+        $execute = DataBase::bdd()->prepare($query);
+        $execute->bindParam(":nom", $stagiaire[0]);
+        $execute->bindParam(":prenom", $stagiaire[1]);
+        $execute->bindParam(':telephone', $stagiaire[2]);
+        $execute->bindParam(':mail', $stagiaire[3]);
+        $execute->bindParam(":adresse", $stagiaire[4]);
+        $execute->bindParam(':cp', $stagiaire[5]);
+        $execute->bindParam(':ville', $stagiaire[6]);
+        $execute->bindParam(':accepter', $stagiaire[7]);
         return $execute->execute();
     }
 }
