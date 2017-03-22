838b431b92069837146faebb7cfcc4c314c53c3d
RomainMustang <romain.g@codeur.online>
2017-03-22 11:15:31 +0100

    Update database.class.php

--- models/database.class.php
+++ models/database.class.php
@@ -1,19 +1,26 @@
 <?php
 
 class DataBase {
+    
     public static function bdd() {
         $host   = "127.0.0.1";
         $base   = "projetOnline";
         $login  = "root";
         $pass    = "";
-
         try {
-            $bdd = new PDO('mysql:host='. $host .';dbname='. $base, $login, $pass);
-            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        } catch (Exception $e) {
+            $bdd = new PDO("mysql:host=$host;dbname=$base", $login, $pass,
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+                ]
+            );
+        } 
+        catch (exception $e) {
             die('Erreur : '. $e->getMessage());
         }
         return $bdd;
     }
 }
+
+?>
