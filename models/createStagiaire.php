304386dd7fc7e42453f7905506da13b8a41f68cf
RomainMustang <romain.g@codeur.online>
2017-03-22 09:44:52 +0100

    Update createStagiaire.php

--- models/createStagiaire.php
+++ models/createStagiaire.php
@@ -24,6 +24,37 @@ class Stagiaire {
         $fetch  = $query->fetchAll();
         return sizeof($fetch) > 1 ? $fetch : false;
     }
+    
+    public static function modifStagiaire($stagiaire){
+        $query  = "UPDATE stagiaires SET cp = :cp,
+                                         ville = :ville,
+                                         mail = :mail,
+                                         telephone = :telephone,
+                                         adresse = :adresse,
+                                         accepter = :accepter
+                       WHERE nom = :nom AND prenom = :prenom";
+
+        $execute = DataBase::bdd()->prepare($query);
+        $execute->bindParam(":nom", $stagiaire[0]);
+        $execute->bindParam(":prenom", $stagiaire[1]);
+        $execute->bindParam(':telephone', $stagiaire[2]);
+        $execute->bindParam(':mail', $stagiaire[3]);
+        $execute->bindParam(":adresse", $stagiaire[4]);
+        $execute->bindParam(':cp', $stagiaire[5]);
+        $execute->bindParam(':ville', $stagiaire[6]);
+        $execute->bindParam(':accepter', $stagiaire[7]);
+        return $execute->execute();
+    }
+    
+        public static function rechercheStagiaire($prenom, $nom){
+        $query = DataBase::bdd()->prepare("SELECT * FROM stagiaires WHERE prenom =:prenom AND nom =:nom");
+        $query->bindParam(":prenom", $prenom);
+        $query->bindParam(":nom", $nom);
+        $query->execute();
+
+        $fetch = $query->fetch();
+        return $fetch;
+    }
 
     // Inserer un resultat de test
     public static function InsertResultat($resultat){
