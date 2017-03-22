68ecf4517ac3d83c31089cbc49e56bf6085bac6a
RomainMustang <romain.g@codeur.online>
2017-03-22 14:36:53 +0100

    Update routesPOST.php

--- src/routesPOST.php
+++ src/routesPOST.php
@@ -46,62 +46,101 @@ $app->post('/coordonnees', function($request, $response){
 });
 
 
-$app->post('/stagiaire', function($request, $response)
-{
-    foreach($_POST as $key => $value) {
-        if (empty($_POST[$key])) {
-            return json_encode(['reponse' => "Champs vides"]);
-        }
+$app->post('/stagiaire', function($request, $response){
+
+    if(!empty($_POST['prenomRecherche']) && !empty($_POST['nomRecherche'])){
+        $reponse = 'ok';
+
+        $resultat = Stagiaire::rechercheStagiaire($_POST['prenomRecherche'], $_POST['nomRecherche']);
+        $response->withHeader('Content-type', 'application/json');
+        return $response->withJson(['recherche' => $resultat, 'reponse' => $reponse], 200, JSON_PRETTY_PRINT);
     }
 
-    $resultatTest=[];
-    $resultatTest[0] = $_POST['date'];
-    $resultatTest[1] = $_POST['connuFormation'];
-    $resultatTest[2] = $_POST['age'];
-    $resultatTest[3] = $_POST['prescription'];
-    $resultatTest[4] = $_POST['status'];
-    $resultatTest[5] = $_POST['prescripteur'];
-    $resultatTest[6] = $_POST['contreIndic'];
-    $resultatTest[7] = $_POST['commentaire'];
-    $resultatTest[8] = $_POST['resultatNiveau'];
-    $resultatTest[9] = $_POST['resultatFormation'];
-    $resultatTest[10] = $_POST['resultatExperience'];
-    $resultatTest[11] = $_POST['pointNiveau'];
-    $resultatTest[12] = $_POST['pointFormation'];
-    $resultatTest[13] = $_POST['pointExperience'];
-    $resultatTest[14] = $_POST['commentaire1'];
-    $resultatTest[15] = $_POST['resultatTravail'];
-    $resultatTest[16] = $_POST['resultatCuriosite'];
-    $resultatTest[17] = $_POST['resultatDynamisme'];
-    $resultatTest[18] = $_POST['resultatDiscours'];
-    $resultatTest[19] = $_POST['resultatMobilite'];
-    $resultatTest[20] = $_POST['pointTravail'];
-    $resultatTest[21] = $_POST['pointCuriosite'];
-    $resultatTest[22] = $_POST['pointDynamisme'];
-    $resultatTest[23] = $_POST['pointDiscours'];
-    $resultatTest[24] = $_POST['pointMobilite'];
-    $resultatTest[25] = $_POST['total'];
-    $resultatTest[26] = $_POST['commentaire2'];
-    $resultatTest[27] = $_POST['commentaires2'];
-    $resultatTest[28] = $_POST['resultatMetier'];
-    $resultatTest[29] = $_POST['resultatEntreprise'];
-    $resultatTest[30] = $_POST['resultatProjet'];
-    $resultatTest[31] = $_POST['pointMetier'];
-    $resultatTest[32] = $_POST['pointEntreprise'];
-    $resultatTest[33] = $_POST['pointProjet'];
-    $resultatTest[34] = $_POST['total1'];
-    $resultatTest[35] = $_POST['commentaire3'];
-    $resultatTest[36] = $_POST['commentaires3'];
-    $resultatTest[37] = $_POST['resultatCulture'];
-    $resultatTest[38] = $_POST['pointCulture'];
-    $resultatTest[39] = $_POST['total2'];
-    $resultatTest[40] = $_POST['commentaire4'];
-    $resultatTest[41] = $_POST['NbPoints'];
-    $resultatTest[42] = $_POST['note'];
+    else if(!empty($_POST['nomEntreprise']) && !empty($_POST['adresseEntreprise']) && !empty($_POST['telephoneEntreprise']) && !empty($_POST['nomTuteur'])){
+        $reponse = 'ok';
+        $infoEntreprise = [];
+        $infoEntreprise[0] = $_POST['nomEntreprise'];
+        $infoEntreprise[1] = $_POST['adresseEntreprise'];
+        $infoEntreprise[2] = $_POST['telephoneEntreprise'];
+        $infoEntreprise[3] = $_POST['nomTuteur'];
+        echo json_encode(['reponse' => $reponse]);
+    }
 
-    Stagiaire::InsertResultat($resultatTest);
+    else if(!empty($_POST['nomStagiaire']) && !empty($_POST['prenomStagiaire']) && !empty($_POST['telephoneStagiaire']) && !empty($_POST['mailStagiaire']) && !empty($_POST['adresseStagiaire']) && !empty($_POST['cpStagiaire']) && !empty($_POST['villeStagiaire'])){
+        $reponse = 'ok';
+        $stagiaire = [];
+        $stagiaire[0] = $_POST['nomStagiaire'];
+        $stagiaire[1] = $_POST['prenomStagiaire'];
+        $stagiaire[2] = $_POST['telephoneStagiaire'];
+        $stagiaire[3] = $_POST['mailStagiaire'];
+        $stagiaire[4] = $_POST['adresseStagiaire'];
+        $stagiaire[5] = $_POST['cpStagiaire'];
+        $stagiaire[6] = $_POST['villeStagiaire'];
+        $stagiaire[7] = $_POST['accepter'];
+        if(Stagiaire::rechercheStagiaire($stagiaire[1],$stagiaire[0]) == null){
+            Stagiaire::insertStagiaire($stagiaire);
+        }
+        else{
+            Stagiaire::modifStagiaire($stagiaire);
+        }
+        echo json_encode(['reponse' => $reponse]);
+    }
+    else{
+        foreach($_POST as $key => $value) {
+            if (empty($_POST[$key])) {
+                return json_encode(['reponse' => "Champs vides"]);
+            }
+        }
 
-    echo json_encode(['reponse' => "ok"]);
+        $resultatTest=[];
+        $resultatTest[0] = $_POST['date'];
+        $resultatTest[1] = $_POST['connuFormation'];
+        $resultatTest[2] = $_POST['age'];
+        $resultatTest[3] = $_POST['prescription'];
+        $resultatTest[4] = $_POST['status'];
+        $resultatTest[5] = $_POST['prescripteur'];
+        $resultatTest[6] = $_POST['contreIndic'];
+        $resultatTest[7] = $_POST['commentaire'];
+        $resultatTest[8] = $_POST['resultatNiveau'];
+        $resultatTest[9] = $_POST['resultatFormation'];
+        $resultatTest[10] = $_POST['resultatExperience'];
+        $resultatTest[11] = $_POST['pointNiveau'];
+        $resultatTest[12] = $_POST['pointFormation'];
+        $resultatTest[13] = $_POST['pointExperience'];
+        $resultatTest[14] = $_POST['commentaire1'];
+        $resultatTest[15] = $_POST['resultatTravail'];
+        $resultatTest[16] = $_POST['resultatCuriosite'];
+        $resultatTest[17] = $_POST['resultatDynamisme'];
+        $resultatTest[18] = $_POST['resultatDiscours'];
+        $resultatTest[19] = $_POST['resultatMobilite'];
+        $resultatTest[20] = $_POST['pointTravail'];
+        $resultatTest[21] = $_POST['pointCuriosite'];
+        $resultatTest[22] = $_POST['pointDynamisme'];
+        $resultatTest[23] = $_POST['pointDiscours'];
+        $resultatTest[24] = $_POST['pointMobilite'];
+        $resultatTest[25] = $_POST['total'];
+        $resultatTest[26] = $_POST['commentaire2'];
+        $resultatTest[27] = $_POST['commentaires2'];
+        $resultatTest[28] = $_POST['resultatMetier'];
+        $resultatTest[29] = $_POST['resultatEntreprise'];
+        $resultatTest[30] = $_POST['resultatProjet'];
+        $resultatTest[31] = $_POST['pointMetier'];
+        $resultatTest[32] = $_POST['pointEntreprise'];
+        $resultatTest[33] = $_POST['pointProjet'];
+        $resultatTest[34] = $_POST['total1'];
+        $resultatTest[35] = $_POST['commentaire3'];
+        $resultatTest[36] = $_POST['commentaires3'];
+        $resultatTest[37] = $_POST['resultatCulture'];
+        $resultatTest[38] = $_POST['pointCulture'];
+        $resultatTest[39] = $_POST['total2'];
+        $resultatTest[40] = $_POST['commentaire4'];
+        $resultatTest[41] = $_POST['NbPoints'];
+        $resultatTest[42] = $_POST['note'];
+
+        Stagiaire::InsertResultat($resultatTest);
+
+        echo json_encode(['reponse' => "ok"]);
+    }
 
 });
 
